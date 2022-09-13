7bd737ec01c2b5d2996ecb0bc3e3cf73c08aaffa
Kunal Shah <kunaljshah03@gmail.com>
2022-09-12 20:46:08 -0400

    added code for data class and updated utils.py

---
+++ src/data.py
@@ -0,0 +1,35 @@
+# `Data` is a holder of `rows` and their sumamries (in `cols`).
+from audioop import add
+from utils import csv, rnd, push
+from row import Row
+from cols import Cols
+
+class Data:
+    def __init__(self, src):
+        self.cols = None # summaries of data
+        self.rows = {}  # kept data
+        if   type(src) == "string":
+            csv(src, add(row)) 
+        else:
+            for row in(src or {}):
+                self.add(row)
+        pass
+    
+    def add(self,xs, row):
+        if   not self.cols:
+            self.cols = Cols(xs) 
+        else:
+            row= push(self.rows, xs.cells and xs or Row(xs)) # ensure xs is a Row
+        for todo in {self.cols.x, self.cols.y}:
+            for col in (todo):
+                col.add(row.cells[col.at]) 
+        
+    def stats(self, places,showCols,fun,t,v):
+        showCols, fun = showCols or self.cols.y, fun or "mid"
+        t={} 
+        for col in showCols: 
+            v=fun(col)
+            v=type(v)=="number" and rnd(v,places) or v
+            t[col.name]=v 
+            return t 
+        
