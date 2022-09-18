96013f33feeec42fd46002fb9108685fbd9fab3f
ameyachavan26 <aachava2@ncsu.edu>
2022-09-17 23:16:52 -0400

    added test engine

---
+++ test/test_engine.py
@@ -0,0 +1,45 @@
+from tkinter import Misc
+
+
+class TestEngine:
+
+    eg = {
+        'BAD': bad() 
+    }  
+
+    def __init__(self) -> None:
+        self.fails = 0
+        self.eg = {}
+
+    def runs(k):
+        if k not in eg:
+            return 
+        old = {}
+        for key, value in the.items():
+            old[key] = value
+        if the['dump']:
+            status == True
+           # out = pass //Todo
+        else:
+            status == False
+            #//Apply some error catching
+        for key, value in old.items():
+            the[key] = value
+        msg = status and ((out == True and 'PASS') or 'FAIL') or 'CRASH'
+        print("!!!!!!", msg, k, status)
+        return out or err 
+
+       
+    def bad(self, eg, runs, fails):
+        '''
+        Test that the test happes when something crashes?
+
+        '''
+       
+        print(eg['dne'])
+
+    def list(self, eg, runs, fails):
+        print("\nExamples lua csv -e ...")
+        for key, values in eg.items():
+            print("\t{key}".format(key = key))
+        return True
